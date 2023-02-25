01da07a76cafcd2f1a28e1f832683852ee596e81
toni-santos <a.oliveirasantos122@gmail.com>
2023-02-25 15:18:55 +0000

    changed the link on the profile picture and name

---
+++ content-scripts/src/modules/utilities/sigarra.js
@@ -0,0 +1,22 @@
+/**
+ * Checks if user is authenticated
+ * 
+ * @returns true if user is authenticated, false otherwise
+ */
+export const isAuth = async () => {
+  const authDiv = document.getElementsByClassName("autenticado");
+  return authDiv.length > 0;
+};
+
+/**
+ * Fetches the user's "up" number
+ * 
+ * @returns the "up" number unique to each user
+ */
+export const getUP = async () => {
+  if (isAuth()) {
+    const pfp = document.querySelector(".autenticado > .fotografia > img");
+
+    return pfp.src.substr(-9, pfp.src.length);
+  }
+};
