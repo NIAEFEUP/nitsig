557234b790e4f175f898e8c774680981aa72cda2
Mariana <73724268+golangis@users.noreply.github.com>
2023-03-06 11:14:12 +0000

    links on the academic journey table switched totally

--- content-scripts/src/modules/profile.js
+++ content-scripts/src/modules/profile.js
@@ -50,12 +50,27 @@ const reduceTableAcademicJourney = () => {
     document.querySelector("#tabelapercurso .totais.n").style.display="none";
 }
 
-const linkToResults = () => {
-    document.querySelectorAll("#tabelapercurso td.n").forEach( elem => {
-        elem.href = ""
-    });
+const linkToGradeResults = () => {
+    document.querySelectorAll("#tabelapercurso tr:is(.i, .p)").forEach(elem => {
+        elem.querySelectorAll("td.n").forEach(e1 => {
+            if (e1.classList.contains("aprovado") || (e1.classList.contains("nao-aprovado"))) {
+                e1.classList.add("cursormao");
+                e1.onclick = (_) => { elem.querySelector("td a.unidade-curricular").click() };
+            }
+        })
+    })
 }
 
+const linkToCurrUnit = () => {
+    document.querySelectorAll("#tabelapercurso tr:is(.i, .p)").forEach(elem => {
+        elem.querySelectorAll("td.k.t.uc").forEach(e1 => {
+            e1.onclick = (e) => { 
+                if (e.isTrusted)
+                elem.querySelector("td.k.t a").click()
+             };
+        })
+    })
+}
 
 export const profileChanges = () => {
 
@@ -69,5 +84,7 @@ export const profileChanges = () => {
         defaultCurrentYear();
         titleTableAcademicJourney();
         reduceTableAcademicJourney();
+        linkToGradeResults();
+        linkToCurrUnit();
     }
 }
