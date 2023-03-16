b9ad3fea69dbb4c390f0b0e613c9b614ade7a3a3
toni-santos <a.oliveirasantos122@gmail.com>
2023-03-16 10:04:52 +0000

    fixed coloring on legend

--- content-scripts/src/modules/schedule.js
+++ content-scripts/src/modules/schedule.js
@@ -1,4 +1,7 @@
-export const improveSchedule = () => {
+import { injectOverrideFunctions } from "./initialize";
+import removeElement from "./utilities/removeElement";
+
+const improveSchedule = () => {
     /** @type {HTMLTableElement} */
     const scheduleElem = document.querySelector(".horario");
 
@@ -93,3 +96,105 @@ const fixClasses = (table) => {
         e.replaceChildren(wrapper);
     })
 };
+
+const createWeekDropdown = async () => {
+    const blocks  = document.querySelectorAll('td.sem-quebra > a');
+
+    const inputWrapper = document.createElement('div');
+    const label = document.createElement('label', {
+        for: 'semanas-select',
+    });
+    const select = document.createElement('select', {
+        name: 'semanas-select',
+    });
+    
+    inputWrapper.className = 'dropdown-wrapper';
+    label.textContent = 'Semanas ';
+    select.name = 'semanas-select';
+    label.for = 'semanas-select';
+    
+    blocks.forEach((block) => {
+        let content = block.textContent;
+        let href = block.href;
+        let opt = document.createElement('option');
+        opt.value = href;
+        opt.innerText = content;
+        if (window.location.href == href) {
+            opt.selected = true;
+        }
+        select.appendChild(opt);
+    });
+
+    select.addEventListener('change', (event) => {
+        window.location.replace(event.target.value);
+    });
+
+    inputWrapper.appendChild(label);
+    inputWrapper.appendChild(select);
+
+    const dropdown = document.querySelector('table.tabela ~ h3');
+    dropdown.replaceWith(inputWrapper);
+
+}
+
+const createLegend = async () => {
+    const oldLegend = document.querySelector('#conteudoinner > table.tabela');
+    
+    const newLegend = document.createElement('div');
+    newLegend.id = 'new-legend';
+    
+    const classTypes = ["Teórica", "Teórico-Prática", "Prática", "Laboratório", "Orientação Tutorial", "Prática Laboratorial", "Almoço"];
+    const classAbbrv = ["TE", "TP", "P", "PL", "OT", "PL"]
+    for (const [index, element] of classTypes.entries()) {
+        const classDiv = document.createElement("div");
+        classDiv.className = "legend-class-item";
+        const abbrv = document.createElement("p");
+        abbrv.id = `legend-abbrv-${index}`;
+        classDiv.innerHTML = element;
+        if (element != "Almoço") {
+            classDiv.id = classAbbrv[index];
+            abbrv.innerHTML = classAbbrv[index];
+        } else {
+            classDiv.id = 'almoco';
+        }
+
+        classDiv.appendChild(abbrv);
+        newLegend.appendChild(classDiv);
+        
+    }
+    
+    oldLegend.replaceWith(newLegend);
+
+}
+
+const createYearPeriodDropdown = async () => {
+    const yearsSelect = document.querySelector('#conteudoinner > form > table > tbody > tr:nth-child(1) > td:nth-child(2) > select');
+    const periodSelect = document.querySelector('#conteudoinner > form > table > tbody > tr:nth-child(2) > td:nth-child(2) > select');
+    const oldForm = document.querySelector('#conteudoinner > form');
+    const div = document.createElement('div');
+    const url = new URL(window.location.href);
+    div.id = 'year-period';
+    div.appendChild(yearsSelect);
+    div.appendChild(periodSelect);
+    yearsSelect.addEventListener('change', (event) => {
+        url.searchParams.set('pv_ano_lectivo', event.target.value)
+        console.log(url);
+        window.location.replace(url);
+    });
+    periodSelect.addEventListener('change', (event) => {
+        url.searchParams.set('pv_periodos', event.target.value)
+        console.log(url);
+        window.location.replace(url);
+    });
+
+    oldForm.replaceWith(div);
+    removeElement('#conteudoinner > form > table > tbody > tr:nth-child(1) > td:nth-child(2) > select');
+
+}
+
+export const changeSchedule = async () => {
+    createWeekDropdown();
+    createLegend();
+    createYearPeriodDropdown();
+    improveSchedule();
+}
