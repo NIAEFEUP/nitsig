f06a6367287d919e9db925ac164de9720a7b9b7e
toino <joaoapereira21@hotmail.com>
2023-03-17 01:55:46 +0000

    Handle overlapping classes, improve schedule style

--- content-scripts/src/modules/schedule.js
+++ content-scripts/src/modules/schedule.js
@@ -1,5 +1,32 @@
 import { injectOverrideFunctions } from "./initialize";
+import { lcmAll } from "./utilities/math";
 import removeElement from "./utilities/removeElement";
+
+const CLASS_TYPE_TO_ABBR = {
+    "Teórica": "TE",
+    "Teórico-Prática": "TP",
+    "Prática": "P",
+    "Laboratório": "PL",
+    "Orientação Tutorial": "OT",
+    "Prática Laboratorial": "PL",
+    "Almoço": "almoco",
+};
+const CLASS_TYPE_TO_ABBR_OVERLAPPING = {
+    "Teórica": "T",
+    "Teórico-Prática": "TP",
+    "Prática": "P",
+    "Laboratório": "PL",
+    "Orientação Tutorial": "OT",
+    "Prática Laboratorial": "PL"
+};
+const CLASS_ABBR_TO_ABBR = {
+    "T": "TE",
+    "TP": "TP",
+    "P": "P",
+    "PL": "PL",
+    "OT": "OT",
+    "PL": "PL"
+};
 
 const improveSchedule = () => {
     /** @type {HTMLTableElement} */
@@ -9,8 +36,8 @@ const improveSchedule = () => {
     if (!scheduleElem) return;
 
     const layout = document.querySelector("#conteudoinner");
-    const title = layout.querySelector("h1");
-    title.innerText = title.innerText.replace(/\s+de\s+.+/, "");
+    /** @type {HTMLTableElement} */
+    const overlapping = document.querySelector("table.dados");
 
     layout
         .querySelectorAll(":scope > :is(h2, h3, table)")
@@ -18,8 +45,9 @@ const improveSchedule = () => {
 
     layout.append(scheduleElem);
 
-    fixScheduleTable(scheduleElem);
     fixClasses(scheduleElem);
+    fixScheduleTable(scheduleElem);
+    fixOverlappingClasses(scheduleElem, overlapping);
 };
 
 /**
@@ -48,7 +76,13 @@ const fixScheduleTable = (table) => {
         colgroup.append(col);
     }
 
-    // Only show time on 1 hour intervals
+    // Change almoço to target the row itself
+    table.querySelectorAll(".almoco").forEach((e) => {
+        e.parentElement.classList.add("almoco");
+        e.classList.remove("almoco");
+    });
+
+    // Add another row at the start
     const body = table.querySelector("tbody");
     const newRow = document.createElement("tr");
 
@@ -58,22 +92,58 @@ const fixScheduleTable = (table) => {
         cell.innerHTML = "&nbsp;"; // ew
         newRow.append(cell);
     }
-
     body.insertBefore(newRow, body.firstElementChild);
 
-    table
-        .querySelectorAll("td.k")
-        .forEach((/** @type {HTMLTableCellElement} */ e) => {
-            if (/^\d{2}:00/.test(e.innerText)) {
-                e.innerText = e.innerText.substring(0, 5);
-                e.rowSpan = 2;
-                // Move to previous row
-                const row = e.parentElement.previousElementSibling;
-                row.insertBefore(e, row.firstElementChild);
-            } else {
-                e.remove();
+    // Remove time headers
+    table.querySelectorAll("td.k").forEach((e) => e.remove());
+
+    /** @type {int[][]} */
+    const rows = [];
+
+    body.querySelectorAll("tr").forEach((e, i) => {
+        rows[i] ??= [1, 2, 3, 4, 5, 6];
+
+        // Add column info to cells (useful later)
+        e.querySelectorAll("td").forEach((c, j) => {
+            const weekday = rows[i].shift();
+            c.dataset.seWeekday = weekday;
+            c.dataset.seRows = `${i}`;
+
+            for (let k = i + 1; k < i + c.rowSpan; ++k) {
+                rows[k] = (rows[k] ?? [1, 2, 3, 4, 5, 6]).filter(
+                    (x) => x != weekday
+                );
+                c.dataset.seRows += ` ${k}`;
             }
         });
+
+        // Replace time header with actual header
+        const timeHeader = document.createElement("th");
+        timeHeader.scope = "row";
+
+        const hour = String(Math.floor((i + 1)/2) + 7).padStart(2, "0");
+        const minute = i%2 ? "00" : "30";
+
+        const span = document.createElement("span");
+        span.innerText = `${hour}:${minute}`;
+        span.classList.add("acs");
+        timeHeader.append(span);
+        
+        if (minute == "00") 
+            timeHeader.dataset.seHourRule = `${hour}:${minute}`;
+
+        e.insertBefore(timeHeader, e.firstElementChild);
+    })
+
+    // Add borders between columns before adding overlapping classes
+    body.querySelectorAll("td").forEach((e) => e.classList.add("column-start"));
+};
+
+const createClass = (name, clazz, room, teacher) => {
+    const wrapper = document.createElement("div");
+    wrapper.append(name, clazz, room, teacher);
+
+    return wrapper;
 };
 
 /**
@@ -90,11 +160,150 @@ const fixClasses = (table) => {
         const classRoom = e.querySelector("table td:first-of-type a");
         const classTeacher = e.querySelector("table td:last-of-type a");
 
-        const wrapper = document.createElement("div");
-        wrapper.append(className, classClass, classRoom, classTeacher);
-
-        e.replaceChildren(wrapper);
+        e.replaceChildren(createClass(className, classClass, classRoom, classTeacher));
     })
+};
+
+const getClassDuration = async (url) => {
+    const r = await fetch(url);
+
+    const decoder = new TextDecoder(
+        r.headers.get("Content-Type").replace("text/html; charset=", "")
+    );
+    const text = decoder.decode(await r.arrayBuffer());
+
+    const parser = new DOMParser();
+    const html = parser.parseFromString(text, "text/html");
+
+    const ret = {};
+
+    html.querySelectorAll(".horas .formulario tr").forEach(
+        (/** @type {HTMLTableRowElement} */ e) => {
+            const type =
+                CLASS_TYPE_TO_ABBR_OVERLAPPING[
+                    e.cells[0].innerText.trim().replace(/s?:$/, "")
+                ];
+            const time = parseFloat(e.cells[1].innerText.replace(",", "."));
+            ret[type] = time;
+        }
+    );
+
+    return ret;
+};
+
+/**
+ * @param {HTMLTableElement} table
+ * @param {HTMLTableElement} overlapping
+ */
+const fixOverlappingClasses = async (table, overlapping) => {
+    // I hate sigarra so much
+    /** @type {Record<string, Record<string, number>>} */
+    const durationCache = {};
+
+    /** @type {Record<string, number} */
+    const weekdays = {};
+    /** @type {NodeListOf<HTMLTableCellElement>} */
+    const headers = table.querySelectorAll("thead th");
+    headers.forEach((e, i) => (weekdays[e.innerText.trim()] = i));
+
+    console.log(weekdays);
+
+    for (const e of overlapping.querySelectorAll("tr")) {
+        // Skip "headers"
+        if (e.querySelector("th")) continue;
+
+        // Get class information
+        /** @type {HTMLAnchorElement} */
+        const className = e.querySelector("[headers=t1] a");
+        const classType = /\((.+)\)/.exec(
+            e.querySelector("[headers=t1]").innerText
+        )[1];
+        /** @type {string} */
+        const weekday = weekdays[e.querySelector("[headers=t2]").innerText.trim()];
+        /** @type {string} */
+        const startingTime = e.querySelector("[headers=t3]").innerText;
+        const classRoom = e.querySelector("[headers=t4] a");
+        const classTeacher = e.querySelector("[headers=t5] a");
+        const classClass = e.querySelector("[headers=t6] a");
+
+        const classDuration = (durationCache[className] ??=
+            await getClassDuration(className.href))[classType] * 2;
+
+        const row =
+            (parseInt(startingTime.slice(0, startingTime.indexOf(":"))) - 7) *
+                2 +
+            (startingTime.slice(startingTime.indexOf(":")) == "30");
+
+        // Create class cell with the right info and insert it
+        const cell = document.createElement("td");
+        cell.rowSpan = classDuration;
+        cell.classList.add(CLASS_ABBR_TO_ABBR[classType]);
+        cell.append(
+            createClass(className, classClass, classRoom, classTeacher)
+        );
+        cell.dataset.seWeekday = weekday;
+        cell.dataset.seRows = row;
+        for (let i = row + 1; i < row + classDuration; ++i) {
+            cell.dataset.seRows += ` ${i}`;
+        }
+
+        const tr = table.querySelector(`tr:nth-of-type(${row + 1})`);
+        let next = null;
+
+        for (let i = weekday + 1; i < 7; ++i) {
+            next = tr.querySelector(`[data-se-weekday="${i}"]`);
+            if (next) break; 
+        }
+
+        tr.insertBefore(cell, next);
+    }
+
+    // Find the number of columns needed per weekday
+    const set = new Set();
+    for (let i = 1; i < 7; ++i) {
+        for (let j = 0; j < 30; ++j) {
+            const o = table.querySelectorAll(`[data-se-weekday="${i}"][data-se-rows~="${j}"]`);
+            set.add(o.length);
+        }
+    }
+
+    const span = lcmAll(...set);
+
+    // Resize columns to the right size
+    headers.forEach((e, i) => i && (e.colSpan = span));
+    table.querySelectorAll("col").forEach((e, i) => i && (e.span = span));
+    table.querySelectorAll("[data-se-weekday][data-se-rows]").forEach((e) => e.colSpan = span);
+    for (let i = 1; i < 7; ++i) {
+        for (let j = 0; j < 30; ++j) {
+            const o = table.querySelectorAll(`[data-se-weekday="${i}"][data-se-rows~="${j}"]`);
+            o.forEach((c) => c.colSpan = Math.min(span/o.length, c.colSpan));
+        }
+    }
+
+    // Insert spaces where needed
+    // FIXME: Still not perfect, in this page the space in row 4 should be inserted first, not last
+    // https://sigarra.up.pt/feup/pt/HOR_GERAL.UCURR_VIEW?pv_ocorrencia_id=501662&pv_ano_lectivo=2022&pv_periodos=2
+    for (let i = 1; i < 7; ++i) {
+        for (let j = 0; j < 30; ++j) {
+            const o = table.querySelectorAll(`[data-se-weekday="${i}"][data-se-rows~="${j}"]`);
+            let s = 0;
+            o.forEach((c) => s += c.colSpan);
+            
+            if (s < span) {
+                const space = document.createElement("td");
+                space.colSpan = span - s;
+
+                const tr = table.querySelector(`tr:nth-of-type(${j + 1})`);
+                let next = null;
+                for (let k = i + 1; k < 7; ++k) {
+                    next = tr.querySelector(`[data-se-weekday="${k}"]`);
+                    if (next) break; 
+                }
+
+                tr.insertBefore(space, next);
+            }
+        }
+    }
 };
 
 const createWeekDropdown = async () => {
@@ -143,24 +352,19 @@ const createLegend = async () => {
     const newLegend = document.createElement('div');
     newLegend.id = 'new-legend';
     
-    const classTypes = ["Teórica", "Teórico-Prática", "Prática", "Laboratório", "Orientação Tutorial", "Prática Laboratorial", "Almoço"];
-    const classAbbrv = ["TE", "TP", "P", "PL", "OT", "PL"]
-    for (const [index, element] of classTypes.entries()) {
+    for (const type of Object.keys(CLASS_TYPE_TO_ABBR)) {
         const classDiv = document.createElement("div");
         classDiv.className = "legend-class-item";
+
         const abbrv = document.createElement("p");
-        abbrv.id = `legend-abbrv-${index}`;
-        classDiv.innerHTML = element;
-        if (element != "Almoço") {
-            classDiv.id = classAbbrv[index];
-            abbrv.innerHTML = classAbbrv[index];
-        } else {
-            classDiv.id = 'almoco';
+        classDiv.innerHTML = type;
+        classDiv.id = CLASS_TYPE_TO_ABBR[type];
+        if (type != "Almoço") {
+            abbrv.innerHTML = CLASS_TYPE_TO_ABBR[type];
         }
 
         classDiv.appendChild(abbrv);
         newLegend.appendChild(classDiv);
-        
     }
     
     oldLegend.replaceWith(newLegend);
