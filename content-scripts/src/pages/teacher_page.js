142e66db35f5c6bbe35b04e25fb056b27861ffcc
Luís Duarte <luis.duarte.2003.13@hotmail.com>
2023-05-01 00:35:16 +0100

    Finish the page, removes keypair tables and better groups elements

--- content-scripts/src/pages/teacher_page.js
+++ content-scripts/src/pages/teacher_page.js
@@ -17,12 +17,153 @@ export const teacherPage = () => {
     makePublicationWebsiteButtons();
     groupSectionTitleAndContent(".informacao-pessoal-funcoes");
     groupSectionTitleAndContent(".informacao-pessoal-dados-dados");
-    makeWebsiteButtonIfExistis();
+    makeWebsiteButtonIfExists();
+
+    tagGroupedElements();
+
+    //nuke the html structure, leaving only the elements that contain the information
+    removeElementWithoutChildren(".informacao-pessoal-dados-dados");
+    removeElementWithoutChildren(".informacao-pessoal-dados");
+    removeElementWithoutChildren(".informacao-pessoal-funcoes");
+
+
+    //we group the page contents to be easier to style to
+    groupChildrenBySelector([".teacherTitleBar", ".tabelasz", ".websiteButton"],
+        ["mainInfoContent"]);
+    
+    groupChildrenBySelector([".informacao-pessoal-dados-foto", ".mainInfoContent"], 
+        ["mainInfoRow"]);
+
+    groupChildrenBySelector([".contact-info", ".roles", ".publicationWebsiteList"],
+        ["extaInformationRow"]);
+
+    removeTwoColumnTable(".contact-info > table", true);
+
+    //i hate sigarra, for some reason it nests one table inside each other
+    const rolesTable = document.querySelector(".roles > table > tbody > tr > td > table");
+    if(rolesTable !== undefined){
+        document.querySelector(".roles > table").remove();
+        document.querySelector(".roles").appendChild(rolesTable);
+        removeTwoColumnTable(".roles > table", true);
+    }
+
+    const investigationTable = document.querySelector(".informacao-pessoal-outras > table > tbody > tr > td > table");
+    if(investigationTable !== undefined){
+        document.querySelector(".informacao-pessoal-outras > table").remove();
+        document.querySelector(".informacao-pessoal-outras").appendChild(investigationTable);
+        removeTwoColumnTable(".informacao-pessoal-outras > table", true);
+
+    }
+
+
 
 
 };
 
-function makeWebsiteButtonIfExistis(){
+function removeTwoColumnTable(tableSelector, inverted=false){
+    const table = document.querySelector(tableSelector);
+    if(table === undefined || table.tagName !== "TABLE") 
+        throw Error("Couldnt find table with " + tableSelector +  " selector");
+    const tbody = table.children[0];
+    const div = document.createElement("div");
+    div.classList.add("keyPairTable")
+    for(const tr of tbody.children){
+        if(tr.children.length !== 2){
+            throw Error("Table with selector " + tableSelector + " isn't a two column table");
+        }
+        if (tr.children[0].children.length === 0){
+            const p = document.createElement("p");
+            p.textContent = tr.children[0].textContent;
+            if(!inverted) p.classList.add("highlightedPart")
+            p.classList.add("pairStart");
+            div.appendChild(p);
+        } else {
+            const element = tr.children[0].children[0];
+            if(!inverted) element.classList.add("highlightedPart");
+            p.classList.add("pairStart");
+
+            div.append(element);
+        }
+
+
+        if (tr.children[1].children.length === 0){
+            const p = document.createElement("p");
+            p.textContent = tr.children[1].textContent;
+            if(inverted) p.classList.add("highlightedPart");
+            p.classList.add("contentPart");
+            div.appendChild(p);
+            
+        } //sometimes table elements will not also include ::text but also a child element
+          //aka villate's cellphone when the user is logged on :)  
+        else {
+            const innerDiv = document.createElement("div")
+            if(tr.children[1].childNodes[0].nodeValue != ""){
+                const p = document.createElement("p");
+                p.style.display = "inline";
+                p.textContent = tr.children[1].childNodes[0].nodeValue;
+                innerDiv.append(p);
+            }
+            const element = tr.children[1].children[0];
+            if(inverted) innerDiv.classList.add("highlightedPart");
+            innerDiv.classList.add("contentPart");
+            innerDiv.append(element);
+            div.append(innerDiv);
+        }
+        
+    }
+    table.parentElement.appendChild(div);
+    table.remove();
+}
+
+
+function groupChildrenBySelector(childSelectors, classList){
+    if(childSelectors === undefined) return;
+    if(childSelectors.length === 0) return;
+    const parent = document.querySelector(childSelectors[0]).parentElement;
+
+    const groupElement = document.createElement("div");
+
+    parent.insertBefore(groupElement, document.querySelector(childSelectors[0]));
+    if(classList !== undefined) groupElement.classList.add(...classList);
+
+    childSelectors.forEach((childSelector) => {
+        const child = document.querySelector(childSelector);
+        if(child === undefined) return;
+        child.remove();
+        groupElement.appendChild(child);
+    });
+
+}
+
+function tagGroupedElements(){
+    const contacts = document.querySelector(".informacao-pessoal-dados-dados > div");
+    if(contacts !== undefined){
+        contacts.classList.add("contact-info")
+    }
+    const roles = document.querySelector(".informacao-pessoal-funcoes");
+    if(roles !== undefined){
+        if(roles.childElementCount === 1){
+            roles.children[0].classList.add("roles");
+        } else if (roles.childElementCount > 1){
+            roles.children[0].classList.add("roles");
+            roles.children[1].classList.add("positions");
+        }
+    }
+
+}
+
+function removeElementWithoutChildren(selector){
+    const element = document.querySelector(selector);
+    const parent = element.parentElement;
+    const listOfChildren = [...element.children];
+    listOfChildren.forEach((child)=>{
+        child.remove();
+        parent.insertBefore(child, element);
+    });
+    element.remove();
+}
+
+function makeWebsiteButtonIfExists(){
     const websiteIcon = document
         .querySelector(".informacao-pessoal-dados-dados > table > tbody > tr:nth-child(1) > td:nth-child(2) > a");
     const informationElement = document.querySelector(".informacao-pessoal-dados-dados")
