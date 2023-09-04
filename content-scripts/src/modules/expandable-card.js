a84dece57c015bf6470292ee289634096097ee42
Luís Duarte <luis.duarte.2003.13@hotmail.com>
2023-09-04 22:27:53 +0100

    Finish expandable card

--- content-scripts/src/modules/expandable-card.js
+++ content-scripts/src/modules/expandable-card.js
@@ -1,25 +1,33 @@
 
 /**
  * @this {Element}
- * @param {MouseEvent} ev 
+ * @param {Element} innerContent
+ * @param {Number} max_size
  */
-function toggleExpandableCard(ev) {
-    /** @type {Keyframe[]} */
-    if (this.dataset.expanded == "false") {
-        this.dataset.expanded = "true";
-        this.animate(
-            [{ transform: "rotate(0)" }, { transform: "rotate(180deg)" }], 
-            { duration: 300, fill: "forwards", easing:"ease-in"}
-        )        
-        
-    } else {
-        this.dataset.expanded = "false";
-        this.animate(
-            [{ transform: "rotate(180deg)" }, { transform: "rotate(0)" }], 
-            { duration: 300, fill: "forwards", easing:"ease-in"}
-        )
+const toggleExpandableCard = (innerContent, max_size) => {
 
-    }
+    function clickEvent(ev){
+        /** @type {Keyframe[]} */
+        if (this.dataset.expanded == "false") {
+            this.dataset.expanded = "true";
+            this.animate(
+                [{ transform: "rotate(0)" }, { transform: "rotate(180deg)" }],
+                { duration: 300, fill: "forwards", easing: "ease-in" }
+            );
+            innerContent.style.maxHeight = `${max_size}px`;
+        } else {
+            this.dataset.expanded = "false";
+            this.animate(
+                [{ transform: "rotate(180deg)" }, { transform: "rotate(0)" }],
+                { duration: 300, fill: "forwards", easing: "ease-in" }
+            );
+            innerContent.style.maxHeight = `0px`;
+            
+        }
+
+        
+    } 
+    return clickEvent;
 };
 
 
@@ -40,13 +48,32 @@ export const makeSigarraExpandableCard = (content) => {
         const button = document.createElement("button");
         button.dataset.expanded = false;
         button.classList.add("se-card-expand-button");
-        button.addEventListener("click", toggleExpandableCard)
         const icon = document.createElement("i");
         icon.classList.add("ri-arrow-down-s-line", "ri-2x");
         button.append(icon);
         collapesedCard.append(div, button);
         element.prepend(collapesedCard);
 
+        const innerContent = Array.from(element.children);
+        innerContent.shift();
+        const innerContentDiv = document.createElement("div");
+        innerContentDiv.append(...innerContent);
+        innerContentDiv.classList.add("se-expandable-card-wrapper");
 
+        element.append(innerContentDiv);
+
+        let func = toggleExpandableCard(innerContentDiv, innerContentDiv.offsetHeight);
+        button.addEventListener("click", func);
+        
+        element.addEventListener("resize", (_) => {
+            button.removeEventListener("click", func);
+            const previousMaxHeight = innerContent.style.maxHeight;
+            innerContent.style.maxHeight = "100%";
+            func = toggleExpandableCard(innerContentDiv, innerContentDiv.offsetHeight);
+            innerContent.style.maxHeight = previousMaxHeight;
+            button.addEventListener("click", func);
+        });
+
+        innerContentDiv.style.maxHeight = `0px`;
     });
 };
