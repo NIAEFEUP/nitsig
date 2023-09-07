454674879a656c28b79b5cc24bb27fafbadc5457
thePeras <jtopereira@gmail.com>
2023-09-07 10:23:06 +0100

    Popup suggestion page toast error, and fit height

--- popup/components/layout/Footer.js
+++ popup/components/layout/Footer.js
@@ -3,16 +3,18 @@ import Link from 'next/link'
 const Footer = () => {
   return (
     <footer className="flex items-center justify-center w-full gap-2 px-2 pt-4 pb-8">
+      {/* <button
       <button
         onClick={() => window.close()}
         type="button"
-        className="inline-flex items-center px-4 py-2 text-[15px] font-bold text-white border border-transparent rounded-full shadow-sm bg-accentThree hover:bg-accentFive focus:outline-none w-fit"
+        className="inline-flex items-center px-4 py-2 text-[15px] font-bold text-white border border-transparent rounded-full shadow-sm bg-accentThree hover:bg-accentFive focus:outline-none w-fi duration-200"
       >
-        Feito
+        Fechar
       </button>
+      */}
       <Link href="feedback">
         <button
-          className="inline-flex items-center px-4 py-2 text-[15px] font-bold text-white border border-transparent rounded-full shadow-sm bg-accentThree hover:bg-accentFive focus:outline-none w-fit"
+          className="inline-flex items-center px-4 py-2 text-[15px] font-bold text-white border border-transparent rounded-full shadow-sm bg-accentThree hover:bg-accentFive focus:outline-none w-fit duration-200"
           type="button"
         >
           Dar Feedback
--- popup/pages/feedback.js
+++ popup/pages/feedback.js
@@ -1,6 +1,7 @@
 import Container from "../components/layout/Container"
 import Header from "../components/layout/Header"
 import { useState } from "react"
+import { Toaster, toast } from "react-hot-toast"
 
 const FORMS_LINK = "https://docs.google.com/forms/d/e/1FAIpQLSdvLEl7FzXEtcq-ZeD1Lrm_1S-CTmCBc3-_AwXmWAw1LU3Z9A/formResponse"
 
@@ -12,7 +13,7 @@ const FeedBackPage = () => {
 
   const submitFeedback = async () => {
     if (title.length === 0 || description.length === 0) {
-      return alert("Preencha todos os campos")
+      return toast.error("Preencha todos os campos")
     }
 
     const url = new URL(FORMS_LINK);
@@ -20,8 +21,10 @@ const FeedBackPage = () => {
     url.searchParams.append("entry.1903295036", title)
     url.searchParams.append("entry.1357581566", description)
     fetch(url)
-    
-    alert("Feedback enviado com sucesso!")
+
+    toast.success("Enviado com sucesso!")
+    setTitle("")
+    setDescription("")
   }
 
   return (
@@ -33,7 +36,7 @@ const FeedBackPage = () => {
             onClick={() => setBugReport(false)}
             className={"font-semibold px-4 py-2 rounded-l-3xl transition-colors duration-200 " +
               (bugReport
-                ? "bg-bgTwo dark:bg-bgTwoDark text-accentOne"
+                ? "bg-bgTwo dark:bg-bgTwoDark text-accentOne dark:text-accentOneDark dark:text-opacity-60"
                 : "bg-accentThree text-white ")
             }
           >
@@ -43,7 +46,7 @@ const FeedBackPage = () => {
             onClick={() => setBugReport(true)}
             className={"font-semibold px-4 py-2 pr-6 rounded-r-3xl transition-colors duration-200 " +
               (!bugReport
-                ? "bg-bgTwo dark:bg-bgTwoDark text-accentOne"
+                ? "bg-bgTwo dark:bg-bgTwoDark text-accentOne dark:text-accentOneDark dark:text-opacity-60"
                 : "bg-accentThree text-white ")
             }
           >
@@ -52,13 +55,13 @@ const FeedBackPage = () => {
         </div>
         <label className="flex flex-col gap-1 text-sm font-bold dark:text-accentOneDark text-accentOne">
           Título
-          <input type="text" className="w-full px-4 py-3 text-[15px] font-semibold border border-transparent shadow-sm focus:outline-none dark:bg-bgTwoDark bg-bgTwo rounded-2xl" placeholder="Breve título"
+          <input type="text" className="w-full px-4 py-3 text-[15px] font-semibold border border-transparent shadow-sm focus:outline-none dark:bg-bgTwoDark bg-bgTwo rounded-2xl dark:placeholder-accentOneDark dark:placeholder-opacity-60" placeholder="Breve título"
             value={title} onChange={e => setTitle(e.target.value)}
           />
         </label>
         <label className="flex flex-col gap-1 text-sm font-bold dark:text-accentOneDark text-accentOne">
           Descrição
-          <textarea className="min-h-[8em] w-full px-4 py-3 text-[15px] font-semibold border border-transparent shadow-sm focus:outline-none dark:bg-bgTwoDark bg-bgTwo rounded-2xl" placeholder="Descrição"
+          <textarea className="min-h-[8em] w-full px-4 py-3 text-[15px] font-semibold border border-transparent shadow-sm focus:outline-none dark:bg-bgTwoDark bg-bgTwo rounded-2xl dark:placeholder-accentOneDark dark:placeholder-opacity-60" placeholder="Descrição"
             value={description} onChange={e => setDescription(e.target.value)}
           />
         </label>
@@ -67,17 +70,21 @@ const FeedBackPage = () => {
         <button
           onClick={() => history.back()}
           type="button"
-          className="inline-flex items-center px-4 py-2 text-[15px] font-bold text-white border border-transparent rounded-full shadow-sm bg-accentThree hover:bg-accentFive focus:outline-none w-fit"
+          className="inline-flex items-center px-4 py-2 text-[15px] font-bold text-white border border-transparent rounded-full shadow-sm bg-gray-700 hover:bg-opacity-90 duration-200 focus:outline-none w-fit"
         >
           Cancelar
         </button>
         <button
-          className="inline-flex items-center px-4 py-2 text-[15px] font-bold text-white border border-transparent rounded-full shadow-sm bg-accentThree hover:bg-accentFive focus:outline-none w-fit"
+          className="inline-flex items-center px-4 py-2 text-[15px] font-bold text-white border border-transparent rounded-full shadow-sm bg-accentThree hover:bg-accentFive duration-200 focus:outline-none w-fit"
           type="button" onClick={submitFeedback}
         >
           Submeter
         </button>
       </footer>
+      <Toaster
+        position="bottom-center"
+        reverseOrder={false}
+      />
     </Container>
   )
 }
