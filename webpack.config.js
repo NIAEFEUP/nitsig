152c42c12978d04e61e40c1002cab5bbc0a447b5
toni-santos <a.oliveirasantos122@gmail.com>
2024-07-26 17:37:34 +0100

    enabled typescript support on webpack and added TSX

--- webpack.config.js
+++ webpack.config.js
@@ -99,6 +99,15 @@ const copyDist = async () => {
 const config = {
     name: "base",
     mode: "production",
+    devtool: "inline-source-map",
+    resolve: {
+        // Add `.ts` and `.tsx` as a resolvable extension.
+        extensions: [".ts", ".tsx", ".js"],
+        // Add support for TypeScripts fully qualified ESM imports.
+        extensionAlias: {
+            ".js": [".js", ".ts"],
+        }
+    },
     entry: {
         "base/content-scripts": "./content-scripts/src/index.js",
         "base/background": "./background.js",
@@ -110,9 +119,20 @@ const config = {
     module: {
         rules: [
             {
-                test: /\.[jt]sx?/,
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                loader: "ts-loader",
+                resolve: {
+                    fullySpecified: false,
+                }
+            },
+            {
+                test: /\.[j]sx?/,
                 exclude: /node_modules/,
                 loader: "esbuild-loader",
+                resolve: {
+                    fullySpecified: false,
+                }
             },
         ],
     },
