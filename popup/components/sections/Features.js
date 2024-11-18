baa549353f1536eef73614ddab54012621bd43fa
Leonor2004 <up202205796@up.pt>
2024-11-18 12:55:17 +0000

    help buttons

--- popup/components/sections/Features.js
+++ popup/components/sections/Features.js
@@ -1,9 +1,27 @@
 import Section from "../layout/Section";
 import SwitchControl from "../controls/SwitchControl";
+import { RiQuestionLine } from "@remixicon/react";
+import { Tooltip } from "react-tooltip";
 
 const Layout = () => (
     <Section title="Funcionalidades">
-        <SwitchControl label="Relembrar utilizador" storageKey="autoLogin" />
+        <SwitchControl
+            label={
+                <>
+                    Relembrar utilizador
+                    <span className="question-icon">
+                        <RiQuestionLine data-tooltip-id="autoLogin-tooltip" />
+                    </span>
+                    <Tooltip 
+                        className="tooltip"
+                        id="autoLogin-tooltip" 
+                        content={ <> Depois de ativada a opção, <br />o login deve ser feito pelas credenciais, <br /> não pela autenticação federada. </>} 
+                        place="top"
+                    />
+                </>
+            }
+            storageKey="autoLogin"
+        />
     </Section>
 );
 
--- popup/components/sections/Layout.js
+++ popup/components/sections/Layout.js
@@ -1,11 +1,61 @@
 import Section from "../layout/Section";
 import SwitchControl from "../controls/SwitchControl";
+import { RiQuestionLine } from "@remixicon/react";
+import { Tooltip } from "react-tooltip";
 
 const Layout = () => (
     <Section title="Aspetos Visuais">
-        <SwitchControl label="Ativar barra de navegação" storageKey="navbar" />
-        <SwitchControl label="Esconder atalhos" storageKey="shortcuts" />
-        <SwitchControl label="Mudar fonte de letra" storageKey="font" />
+        <SwitchControl
+            label={
+                <>
+                    Ativar barra de navegação
+                    <span className="question-icon">
+                        <RiQuestionLine data-tooltip-id="navbar-tooltip" />
+                    </span>
+                    <Tooltip 
+                        className="tooltip"
+                        id="navbar-tooltip" 
+                        content={ <> Muda as opções da barra <br /> lateral para a parte superior. </>} 
+                        place="top"
+                    />
+                </>
+            }
+            storageKey="navbar"
+        />
+        <SwitchControl
+            label={
+                <>
+                    Esconder atalhos
+                    <span className="question-icon">
+                        <RiQuestionLine data-tooltip-id="shortcuts-tooltip"/>
+                    </span>
+                    <Tooltip 
+                        className="tooltip"
+                        id="shortcuts-tooltip" 
+                        content={ <> Esconde a opção dos atalhos. </>} 
+                        place="top"
+                    />
+                </>
+            }
+            storageKey="shortcuts"
+        />
+        <SwitchControl
+            label={
+                <>
+                    Mudar fonte de letra
+                    <span className="question-icon">
+                        <RiQuestionLine data-tooltip-id="font-tooltip"/>
+                    </span>
+                    <Tooltip 
+                        className="tooltip"
+                        id="font-tooltip" 
+                        content={ <> Muda a fonte do texto. </>} 
+                        place="top"
+                    />
+                </>
+            }
+            storageKey="font"
+        />
     </Section>
 );
 
