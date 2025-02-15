a0669f35d1cb78e118d7976090dfd4619fb9f8f1
Simão Barbosa <simao.barbosa.05@gmail.com>
2025-02-15 14:49:14 +0000

    ported teacher_page to ts

--- content-scripts/pages/teacher_page.ts
+++ content-scripts/pages/teacher_page.ts
@@ -5,7 +5,7 @@ import {
     removeTwoColumnTable,
 } from "../modules/utilities/pageUtils";
 
-const publicationWebsites = {
+const publicationWebsites: Record<string, { icon: string; text: string }> = {
     "authenticus.pt": { icon: "authenticusID.png", text: "Authenticus ID" },
     "orcid.org": { icon: "orcid.png", text: "ORCID" },
     "cienciavitae.pt": { icon: "cienciaID.png", text: "Ciência ID" },
@@ -13,7 +13,7 @@ const publicationWebsites = {
     "scopus.com": { icon: "scopus.png", text: "Scopus" },
 };
 
-export const teacherPage = () => {
+export const teacherPage = (): void => {
     // TODO: remove this check
     if (!document.location.href.toLowerCase().includes("func_geral.formview")) {
         return;
@@ -59,58 +59,69 @@ export const teacherPage = () => {
     sectionClasses.forEach(reformatTables);
 };
 
-function reformatTables(parentSelector) {
-    let parentElement = document.querySelector(parentSelector);
-    if (parentElement === null || parentElement.children.length === 0) return;
+function reformatTables(parentSelector: string): void {
+    let parentElement = document.querySelector<HTMLElement>(parentSelector);
+    if (!parentElement || parentElement.children.length === 0) return;
+
     const tableList = parentElement.querySelectorAll(
         "table > tbody > tr > td > table",
     );
 
     if (tableList.length !== 0) {
-        parentElement.querySelector("table").remove();
+        parentElement.querySelector("table")?.remove();
         if (tableList.length > 1) {
             const container = document.createElement("div");
             container.classList.add("se-container");
             parentElement.appendChild(container);
             parentElement = container;
         }
-        for (let table of tableList) {
-            document.querySelector(parentSelector).appendChild(table);
+        for (const table of Array.from(tableList)) {
+            document.querySelector(parentSelector)?.appendChild(table);
             removeTwoColumnTable(
                 `${parentSelector} > table`,
                 true,
-                parentElement,
+                parentElement!, // Non-null assertion here
             );
         }
     } else {
         const tableSelector = `${parentSelector} > table`;
         const table = document.querySelector(tableSelector);
-        parentElement.appendChild(table);
-        removeTwoColumnTable(tableSelector, true, parentElement);
+        if (table) {
+            parentElement.appendChild(table);
+            removeTwoColumnTable(
+                tableSelector,
+                true,
+                parentElement!, // Non-null assertion here
+            );
+        }
     }
 }
 
-function tagGroupedElements() {
-    let contacts = document.querySelector(
+function tagGroupedElements(): void {
+    let contacts = document.querySelector<HTMLElement>(
         ".informacao-pessoal-dados-dados > div:not(.se-website-button)",
     );
     if (contacts !== null) {
         contacts.classList.add("se-contact-info");
     } else {
-        contacts = document.querySelector(
+        contacts = document.querySelector<HTMLElement>(
             ".informacao-pessoal-dados-dados > table:not(.tabelasz)",
         );
-        const div = document.createElement("div");
-        const informacao_pessoal = document.querySelector(
-            ".informacao-pessoal-dados-dados",
-        );
-        const h3 = document.createElement("h3");
-        h3.textContent = "Contactos";
-        div.append(h3, contacts);
-        div.classList.add("se-contact-info");
-        informacao_pessoal.appendChild(div);
+        if (contacts) {
+            const div = document.createElement("div");
+            const informacao_pessoal = document.querySelector<HTMLElement>(
+                ".informacao-pessoal-dados-dados",
+            );
+            const h3 = document.createElement("h3");
+            h3.textContent = "Contactos";
+            div.append(h3, contacts);
+            div.classList.add("se-contact-info");
+            informacao_pessoal?.appendChild(div);
+        }
     }
-    const roles = document.querySelector(".informacao-pessoal-funcoes");
+    const roles = document.querySelector<HTMLElement>(
+        ".informacao-pessoal-funcoes",
+    );
     if (roles !== null) {
         if (roles.childElementCount === 1) {
             roles.children[0].classList.add("se-roles");
@@ -121,14 +132,14 @@ function tagGroupedElements() {
     }
 }
 
-function makeWebsiteButtonIfExists() {
-    const websiteIcon = document.querySelector(
+function makeWebsiteButtonIfExists(): void {
+    const websiteIcon = document.querySelector<HTMLAnchorElement>(
         ".informacao-pessoal-dados-dados > table > tbody > tr:nth-child(1) > td:nth-child(2) > a",
     );
-    const informationElement = document.querySelector(
+    const informationElement = document.querySelector<HTMLElement>(
         ".informacao-pessoal-dados-dados",
     );
-    if (websiteIcon === null) return;
+    if (websiteIcon === null || !informationElement) return;
 
     const websiteLink = websiteIcon.href;
     const websiteButton = document.createElement("a");
@@ -140,22 +151,27 @@ function makeWebsiteButtonIfExists() {
     websiteIcon.remove();
 }
 
-function makePublicationWebsiteButtons() {
-    const tabelasz = document.querySelector(".tabelasz > tbody");
-    const informacaoPessoal = document.querySelector(
+function makePublicationWebsiteButtons(): void {
+    const tabelasz =
+        document.querySelector<HTMLTableSectionElement>(".tabelasz > tbody");
+    const informacaoPessoal = document.querySelector<HTMLElement>(
         ".informacao-pessoal-dados",
     );
+    if (!tabelasz || !informacaoPessoal) return;
     const websiteList = document.createElement("div");
     websiteList.classList.add("se-publication-website-list");
-    const listOfRows = [...tabelasz.children];
+    const listOfRows = Array.from(tabelasz.children) as HTMLElement[];
+
     listOfRows.forEach((row) => {
-        const linkElement = row.querySelector("td:nth-child(2) > a");
+        const linkElement = row.querySelector<HTMLAnchorElement>(
+            "td:nth-child(2) > a",
+        );
         if (linkElement === null) {
             return;
         }
         const link = linkElement.href;
         let found = false;
-        for (let website of Object.keys(publicationWebsites)) {
+        for (const website of Object.keys(publicationWebsites)) {
             if (link.includes(website)) {
                 found = true;
                 const image = document.createElement("img");
@@ -182,43 +198,52 @@ function makePublicationWebsiteButtons() {
     informacaoPessoal.appendChild(websiteList);
 }
 
-function moveResearchSection() {
-    const researchSection = document.querySelector(
+function moveResearchSection(): void {
+    const researchSection = document.querySelector<HTMLElement>(
         ".informacao-pessoal-outras",
     );
-    const informacaoPessoal = document.querySelector(
+    const informacaoPessoal = document.querySelector<HTMLElement>(
         ".informacao-pessoal-dados",
     );
+    if (!researchSection || !informacaoPessoal) return;
     researchSection.remove();
-    const rolesSection = document.querySelector(".informacao-pessoal-funcoes");
-    informacaoPessoal.insertBefore(researchSection, rolesSection);
+    const rolesSection = document.querySelector<HTMLElement>(
+        ".informacao-pessoal-funcoes",
+    );
+    if (rolesSection) {
+        informacaoPessoal.insertBefore(researchSection, rolesSection);
+    }
 }
 
-function makeTitleBar() {
-    const informacaoPessoal = document.querySelector(".informacao-pessoal");
+function makeTitleBar(): void {
+    const informacaoPessoal = document.querySelector<HTMLElement>(
+        ".informacao-pessoal",
+    );
 
-    const titleElement = document.querySelectorAll("#conteudoinner > h1")[1];
+    const titleElement = document.querySelectorAll<HTMLHeadingElement>(
+        "#conteudoinner > h1",
+    )[1];
     const title = titleElement.textContent;
     titleElement.remove();
-    const siglaRow = document.querySelector(
+    const siglaRow = document.querySelector<HTMLTableRowElement>(
         ".tabelasz > tbody:nth-child(1) > tr:nth-child(2)",
     );
-    const sigla = document.querySelector(
+    const sigla = document.querySelector<HTMLBRElement>(
         ".tabelasz > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(2) > b:nth-child(1)",
-    ).textContent;
-    siglaRow.remove();
+    )?.textContent;
+    siglaRow?.remove();
 
     const titleBar = document.createElement("div");
     titleBar.classList.add("se-teacher-title-bar");
 
     const newTitle = document.createElement("h1");
-    newTitle.textContent = title;
+    newTitle.textContent = title || "";
 
     const newSigla = document.createElement("h3");
-    newSigla.textContent = sigla;
+    newSigla.textContent = sigla || "";
 
     titleBar.appendChild(newTitle);
     titleBar.appendChild(newSigla);
 
-    informacaoPessoal.prepend(titleBar);
+    informacaoPessoal?.prepend(titleBar);
 }
