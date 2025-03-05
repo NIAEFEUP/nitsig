d61d791bf4bbfa09e3b6344bca501c7d69518918
martim07833 <up202307833@up.pt>
2025-03-05 12:20:18 +0000

    formatted

--- content-scripts/components/Card.tsx
+++ content-scripts/components/Card.tsx
@@ -8,52 +8,71 @@ interface CardProps {
     button?: Element;
 }
 
-const Card: JSX.Component<CardProps> = ({id, title, description, imgSrc, subtitles, button }) => {
+const Card: JSX.Component<CardProps> = ({
+    id,
+    title,
+    description,
+    imgSrc,
+    subtitles,
+    button,
+}) => {
     let finalClassName = "se-card";
     return (
-        <div className = {finalClassName} id ={id}>
-            <div className = "container">
-            {title && description && imgSrc && subtitles && button ? 
-                <div className = "details">
-                    {imgSrc ?
-                    <img className = "img" src ={imgSrc}>
-                    </img> : " "
-                    }
-                    {title ? 
-                    <h2 className = "title">
-                        {title}
-                    </h2> : " "
-                    }
-                    {(subtitles && subtitles.length > 0) ? 
-                    subtitles.map(subtitle => {return <div className = "subtitles">{subtitle}</div>})
-                    : " "}
-                    {description ?
-                    <p className = "description">
-                        {description}
-                    </p> : ""
-                    }
-                    {button ? button : " "}
-                </div>
-                    :<div className="detailsalt">
-                        {imgSrc ?
-                        <img className="img" src={imgSrc}></img>:" "}
+        <div className={finalClassName} id={id}>
+            <div className="container">
+                {title && description && imgSrc && subtitles && button ? (
+                    <div className="details">
+                        {imgSrc ? (
+                            <img className="img" src={imgSrc}></img>
+                        ) : (
+                            " "
+                        )}
+                        {title ? <h2 className="title">{title}</h2> : " "}
+                        {subtitles && subtitles.length > 0
+                            ? subtitles.map((subtitle) => {
+                                  return (
+                                      <div className="subtitles">
+                                          {subtitle}
+                                      </div>
+                                  );
+                              })
+                            : " "}
+                        {description ? (
+                            <p className="description">{description}</p>
+                        ) : (
+                            ""
+                        )}
+                        {button ? button : " "}
+                    </div>
+                ) : (
+                    <div className="detailsalt">
+                        {imgSrc ? (
+                            <img className="img" src={imgSrc}></img>
+                        ) : (
+                            " "
+                        )}
                         <div className="other">
                             <div className="textbox">
-                                {title ? <h2 className="title">{title}</h2>:" "}
-                                {description ? <p className="description">{description}</p>:" "}
+                                {title ? (
+                                    <h2 className="title">{title}</h2>
+                                ) : (
+                                    " "
+                                )}
+                                {description ? (
+                                    <p className="description">{description}</p>
+                                ) : (
+                                    " "
+                                )}
                             </div>
                             <div className="buttonbox">
-                                {button?button:" "}
+                                {button ? button : " "}
                             </div>
                         </div>
                     </div>
-            }
-
+                )}
             </div>
-
         </div>
-        
-    )
+    );
 };
 
 export default Card;
